45d66ced760c17fc31b36a4182d8bc5796bc6c90
root786kali <115746692+root786kali@users.noreply.github.com>
2023-02-14 04:14:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContens = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContens.forEach(tc =>{ /*tc =tabcontent*/
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+
+        tabs.forEach(t =>{/* t =tab */
+            t.classList.remove('filter-tab-active')
+        })
+tab.classList.add('filter-tab-active')
+        
+
+        
+    })
+})      
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
